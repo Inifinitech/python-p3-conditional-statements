acdf2c4c52c296e77a6a20b851df7452d04b5f4d
Kush <kuriaisac@gmail.com>
2024-08-28 08:22:38 +0300

    updates changes

--- js/index.js
+++ js/index.js
@@ -52,7 +52,7 @@ function fizzbuzz(num) {
 /*
   Write a method `calculator` that takes three arguments: an operation and two
   numbers. If the operation is one of the following: `+`, `-`, `*`, or `\`,
-  return the value of calling the operation on the two numbers. Otherwise,
+  return the value else:of calling the operation on the two numbers. Otherwise,
   output a message saying "Invalid operation!" and return `nil`.
 */
 function calculator(operation, num1, num2) {
